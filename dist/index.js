81680f920af563c066b01710e4e44753c510e533
GitLabBR <gitlabbrasil@proton.me>
2024-11-20 18:57:47 -0300

    Updated index.js

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('Hello world!');
+const core_1 = require('@actions/core')
+const octokit = require('@actions/github')
+const { execSync } = require('child_process')
+async function run(){
+    const pwd = (0, core_1.getInput)("password")
+    console.log('Upgrading/Installing pip..')
+    execSync('python -m pip install --upgrade pip')
+    console.log('Installig build package..')
+    execSync('python -m pip install build')
+    console.log('Building project..')
+    execSync('python -m build')
+    console.log('Project was built with success!')
+    console.log('Installig twine package..')
+    execSync('python -m pip install twine')
+    console.log('Publishing project...')
+    execSync('python -m twine upload dist/*')
+    console.log('Project published!')
+}
