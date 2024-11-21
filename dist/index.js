e17dc7f1bbd75480eecd76c2ee22bc4d9198d071
GitLabBR <gitlabbrasil@proton.me>
2024-11-21 20:19:48 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ async function run() {
   try {
 
     // Get the input from the workflow
-    const user = process.env.USERNAME
+    const user = process.env.USERNAME;
     const pwd = process.env.PASSWORD;
 
     
