1687509aaacfe88aacc2c331fe4825a372172c6b
GitLabBR <gitlabbrasil@proton.me>
2024-11-21 20:12:47 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,13 @@ async function run() {
   try {
 
     // Get the input from the workflow
-    const token = process.env.TOKEN;
     const pwd = process.env.PASSWORD;
 
     
     console.log("Password input received:", pwd);
     execSync("python -m pip install --upgrade pip", { stdio: "inherit" });
     execSync("python -m pip install twine", { stdio: "inherit" });
-    execSync(`python -m twine upload -u ${token} -p ${pwd} dist/*`, {
+    execSync(`python -m twine upload -u __token__ -p ${pwd} dist/*`, {
       stdio: "inherit",
     });
   } catch (error) {
