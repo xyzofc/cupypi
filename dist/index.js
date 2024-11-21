264f9f86772714a7f6af9b9e9d21a93b76b33fb1
GitLabBR <gitlabbrasil@proton.me>
2024-11-21 18:49:00 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,12 @@ async function run() {
     const pwd = process.env.PASSWORD;
 
     console.log("Password input received:", pwd);
+    const fullRepo = process.env.GITHUB_REPOSITORY; // e.g., "owner/repo"
 
+    const repoName = fullRepo.split("/")[1]; // Extracts the "repo" part
+    
     // Execute the required commands
+    execSync(`cd "/home/runner/work/${repoName}"`)
     execSync("python -m pip install --upgrade pip", { stdio: "inherit" });
     execSync("python -m pip install build", { stdio: "inherit" });
     execSync("python -m build", { stdio: "inherit" });
