321718c4c0b02cb9db9601bacff8897aee045c52
GitLabBR <gitlabbrasil@proton.me>
2024-11-20 21:07:05 -0300

    Commit

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,23 @@
 "use strict";
 import { execSync } from "child_process";
-import { getInput } from "@actions/core";
 
 async function run() {
   try {
-    const pwd = getInput("password");
+    // GitHub Actions define inputs como variáveis de ambiente com prefixo "INPUT_"
+    const pwd = process.env.INPUT_PASSWORD;
+    if (!pwd) {
+      throw new Error("Input 'password' was not provided or is empty.");
+    }
+
     console.log("Password input received:", pwd);
 
     execSync("python -m pip install --upgrade pip", { stdio: "inherit" });
     execSync("python -m pip install build", { stdio: "inherit" });
     execSync("python -m build", { stdio: "inherit" });
     execSync("python -m pip install twine", { stdio: "inherit" });
-    execSync("python -m twine upload dist/*", { stdio: "inherit" });
+    execSync(`python -m twine upload -u __token__ -p ${pwd} dist/*`, {
+      stdio: "inherit",
+    });
   } catch (error) {
     console.error("Error during execution:", error.message);
     process.exit(1);
