d2a5cec2f6c7022ed9379643db72f6a7ee8c16ef
GitLabBR <gitlabbrasil@proton.me>
2024-11-20 21:02:24 -0300

    Commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('Hello world!');
+import { execSync } from "child_process";
+import { getInput } from "@actions/core";
+
+async function run() {
+  try {
+    const pwd = getInput("password");
+    console.log("Password input received:", pwd);
+
+    execSync("python -m pip install --upgrade pip", { stdio: "inherit" });
+    execSync("python -m pip install build", { stdio: "inherit" });
+    execSync("python -m build", { stdio: "inherit" });
+    execSync("python -m pip install twine", { stdio: "inherit" });
+    execSync("python -m twine upload dist/*", { stdio: "inherit" });
+  } catch (error) {
+    console.error("Error during execution:", error.message);
+    process.exit(1);
+  }
+}
+
+run();
